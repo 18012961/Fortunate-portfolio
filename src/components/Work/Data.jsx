7a96891649e6d6fc323165f95b6b140934aee1d7
18012961 <fortunaterefilwe@gmail.com>
2024-05-31 11:34:58 +0200

    first commit

---
+++ src/components/Work/Data.jsx
@@ -0,0 +1,62 @@
+import dior from "../../assets/dior.jpg";
+import clean from "../../assets/cleangirl.jpg";
+import download from "../../assets/download.jpg";
+import hire from "../../assets/HIRE.jpg"
+
+export const projectsData =[
+    {
+        id: 1,
+        image: clean,
+        title: 'Web design',
+        category: 'web',
+        demoLink: 'https://18012961.github.io/Gabimotubawebsite/'
+      },
+{
+    id:2,
+    image: dior,
+    title: 'Web Design',
+    category: 'web',
+    demoLink:'https://18012961.github.io/simplecity.github.io/'
+},
+{
+    id:3,
+    image: clean,
+    title: 'UX design',
+    category: 'design',
+    demoLink:'https://drive.google.com/file/d/1hb1ubj4VnhO4Kjl4tPYN52HTCKQHuSRb/view?usp=sharing'
+},
+{
+    id:4,
+    image: dior,
+    title: 'UX design',
+    category: 'design',
+    demoLink:'https://drive.google.com/file/d/1cenGxyOnVDoRPtazTIOVR10I1c40dA3d/view?usp=sharing'
+},{
+    id:5,
+    image: clean  ,
+    title: 'brand design',
+    category: 'web',
+   
+},
+
+
+];
+
+
+export const projectsNav = [
+{
+    name:'all'
+},
+
+{
+    name:'web'
+},
+{
+    name:'app'
+},
+{
+    name:'design'
+},
+
+
+];
